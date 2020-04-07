3ac9e0b96dce6d0b21a02297d91c9cae616a0e5c
nahkd123 <nahkd123@gmail.com>
2020-04-07 11:08:21 +0700

    alright, time to test

--- src/main/java/me/nahkd/spigot/sfaddons/endrex/handlers/ChunksEventsHandlers.java
+++ src/main/java/me/nahkd/spigot/sfaddons/endrex/handlers/ChunksEventsHandlers.java
@@ -10,17 +10,20 @@ import org.bukkit.event.world.ChunkLoadEvent;
 
 import me.nahkd.spigot.sfaddons.endrex.Endrex;
 import me.nahkd.spigot.sfaddons.endrex.structures.ForestsGenerator;
+import me.nahkd.spigot.sfaddons.endrex.structures.MysteryBoxesGenerator;
 import me.nahkd.spigot.sfaddons.endrex.structures.SpongePoweredHouses;
 
 public class ChunksEventsHandlers implements Listener {
 	
 	static SpongePoweredHouses spongePowered;
 	static ForestsGenerator forestsGenerator;
+	static MysteryBoxesGenerator mysteryBoxexGenerator;
 	static Random random;
 	
 	public ChunksEventsHandlers() {
 		spongePowered = new SpongePoweredHouses();
 		forestsGenerator = new ForestsGenerator();
+		mysteryBoxexGenerator = new MysteryBoxesGenerator();
 		random = new Random();
 	}
 	
@@ -36,6 +39,8 @@ public class ChunksEventsHandlers implements Listener {
 		random.setSeed((chunk.getWorld().getSeed() >> 3) + chunk.getX() + chunk.getZ());
 		spongePowered.generateStructure(chunk.getWorld(), chunk, random);
 		forestsGenerator.generateStructure(chunk.getWorld(), chunk, random);
+		mysteryBoxexGenerator.generateStructure(chunk.getWorld(), chunk, random);
+		
 	}
 	
 }
--- src/main/java/me/nahkd/spigot/sfaddons/endrex/structures/MysteryBoxesGenerator.java
+++ src/main/java/me/nahkd/spigot/sfaddons/endrex/structures/MysteryBoxesGenerator.java
@@ -1,12 +1,20 @@
 package me.nahkd.spigot.sfaddons.endrex.structures;
 
+import java.util.Arrays;
 import java.util.Random;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.World.Environment;
+import org.bukkit.inventory.ItemStack;
 
 import me.nahkd.spigot.sfaddons.endrex.Endrex;
+import me.nahkd.spigot.sfaddons.endrex.schem2.VectorInt;
 import me.nahkd.spigot.sfaddons.endrex.schem2.nahkdSchematic2;
+import me.nahkd.spigot.sfaddons.endrex.schem2.loot.LootTableEntry;
 
 public class MysteryBoxesGenerator extends StructuresGenerator {
 
@@ -17,6 +25,23 @@ public class MysteryBoxesGenerator extends StructuresGenerator {
 	
 	@Override
 	public void generateStructure(World world, Chunk newChunk, Random rand) {
+		if (world.getEnvironment() != Environment.THE_END) return;
+		if (newChunk.getX() >= -32 && newChunk.getZ() >= -32 && newChunk.getX() <= 32 && newChunk.getZ() <= 32) return;
+		int mx = rand.nextInt(16);
+		for (int i = 0; i < mx; i++) rand.nextInt();
+		double hit = rand.nextDouble();
+		if (hit <= 0.25) {
+			int
+				genX = rand.nextInt(13) + (newChunk.getX() * 16),
+				genZ = rand.nextInt(13) + (newChunk.getZ() * 16);
+			// Location loc = new Location(world, genX, 100 + rand.nextInt(32), genZ);
+			Bukkit.broadcastMessage("mystery box " + genX + " - " + genZ);
+			schem.pasteSchematic(world, new VectorInt(genX, 100 + rand.nextInt(32), genZ), Arrays.asList(
+					new LootTableEntry(0.5, new ItemStack(Material.ENDER_PEARL, 3)),
+					new LootTableEntry(0.6, new ItemStack(Material.ENDER_PEARL, 2)),
+					new LootTableEntry(0.7, new ItemStack(Material.ENDER_PEARL, 1))
+					));
+		}
 	}
 
 }
